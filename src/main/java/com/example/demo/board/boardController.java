29294beafdc85545f4e6d78c9df02691dbe07cb6
schiva <schiva@empal.com>
2018-11-02 12:39:01 +0900

    update

--- src/main/java/com/example/demo/board/boardController.java
+++ src/main/java/com/example/demo/board/boardController.java
@@ -1,3 +1,5 @@
+// todo : google firebase 연동.
+// todo : post 로 수신된 데이타( json , form fields )로 DB insert ( 일반 Key, PK 자동증가형 )
 package com.example.demo.board;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
