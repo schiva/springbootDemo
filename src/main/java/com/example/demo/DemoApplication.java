69ef90c99300af3773390e241da6c5c1494e1c94
schiva <schiva@empal.com>
2018-11-09 09:38:15 +0900

    ModelAttribute 를 이용한 VO를 통한 Post 처리 및 한글 UTF-8관련 설정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @MapperScan(value = {"com.example.demo"})
@@ -21,6 +18,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
 
 
 }
